577656b87154d91d50d2e8e4d9435d77c285d605
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-07 00:11:09 +0500

    Update tabnavigation.tsx

--- navigation/tabnavigation.tsx
+++ navigation/tabnavigation.tsx
@@ -5,7 +5,7 @@ import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather'
 import EvilIcons from 'react-native-vector-icons/EvilIcons'
 import AntDesign from 'react-native-vector-icons/AntDesign'
-import Chat from '../android/src/screens/Chat';
+import Chat from '../android/src/screens/Chat';   
 import Profile from '../android/src/screens/Profile';
 import Projects from '../android/src/screens/Project';
 
