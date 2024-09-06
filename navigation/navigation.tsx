5709c77d76abcedfefc378b364eddcd04464709c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-06 14:25:51 +0500

    Update navigation.tsx

--- navigation/navigation.tsx
+++ navigation/navigation.tsx
@@ -9,7 +9,7 @@ import Slider2 from '../android/src/screens/Slider2';
 import Slider3 from '../android/src/screens/Slider3';   
 import { Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
-import Signup from '../android/src/screens/Signup';   
+import Signup from '../android/src/screens/Signup';      
 import TabNavigation from './tabnavigation';   
 import Settings from '../android/src/screens/Settings';
 import AddTask from '../android/src/screens/AddTask';
