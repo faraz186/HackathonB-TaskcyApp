cd592d83c961afaa54330f18dda149a9cfc19251
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-18 02:16:24 +0500

    Update Icon.tsx

--- component/Icon.tsx
+++ component/Icon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';                
+import React from 'react';                   
 import Icon from 'react-native-vector-icons/MaterialIcons';      
 import styles from '../styles/style';   
                      
