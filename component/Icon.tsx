f615766de5ad662d6b6a4e6f01f5229c239870d8
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-29 02:21:26 +0500

    Update Icon.tsx

--- component/Icon.tsx
+++ component/Icon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';        
 import styles from '../styles/style';       
                      
-export default function HMIcon(props: any) {            
+export default function HMIcon(props: any) {              
     const { name, size, color } = props;  
             
     return <Icon name={name} size={size ?? 20} color={color ?? styles._dark} />              
