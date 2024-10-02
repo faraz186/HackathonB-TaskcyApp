6dc694bbaeb9e1165d2b819e481907c0870ccb4b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-03 00:55:31 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';        
 import Navigation from './navigation/navigation';             
-          
+            
 const App = () => {                            
   return <Navigation />;           
 };             
