05db24b2fa1183a62f08ca36380f57e34b2ef523
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-26 00:09:42 +0500

    Update Icon.tsx

--- component/Icon.tsx
+++ component/Icon.tsx
@@ -5,5 +5,5 @@ import styles from '../styles/style';
 export default function HMIcon(props: any) {              
     const { name, size, color } = props;              
                  
-    return <Icon name={name} size={size ?? 20} color={color ?? styles._dark} />                    
+    return <Icon name={name} size={size ?? 20} color={color ?? styles._dark} />                      
 }            
