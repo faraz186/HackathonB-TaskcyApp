ae9adf24560d588fbc32bfa3f76e1af8f9707b81
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-27 13:10:44 +0500

    Update Icon.tsx

--- component/Icon.tsx
+++ component/Icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';               
-import Icon from 'react-native-vector-icons/MaterialIcons';             
+import Icon from 'react-native-vector-icons/MaterialIcons';               
 import styles from '../styles/style';              
                          
 export default function HMIcon(props: any)   
