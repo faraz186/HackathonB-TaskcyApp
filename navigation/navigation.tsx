d4274b3e775f32a65979158b6fdac288ea861728
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-28 10:19:47 +0500

    Update navigation.tsx

--- navigation/navigation.tsx
+++ navigation/navigation.tsx
@@ -26,7 +26,7 @@ const Navigation = ({ navigation }: any) => {
         headerShown: true,
         headerBackVisible: false,
         headerShadowVisible: false,     
-    }
+    }  
     const Noheader = {  
         headerShown: false
     }
